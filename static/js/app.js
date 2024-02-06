8b8cb3ce26656151890b1bb6b3a9a163942f60f4
NosamKram <145081164+NosamKram@users.noreply.github.com>
2024-02-05 18:32:46 -0700

    added plotly code

--- static/js/app.js
+++ static/js/app.js
@@ -21,11 +21,53 @@ function optionChanged(id) {
 }
 
 function demo_populate(id) {
-    console.log("demo_populate");
+    var metadata = data.metadata.find(obj=>obj.id==id)
+    demo_el.html("")
+    Object.entries(metadata).forEach(element => {
+        var info = `${element[0]}: ${element[1]}`
+        demo_el.append("p").text(info)
+    });
 }
 
 function graphs(id) {
-    console.log("graphs");
+    var samples = data.samples.find(obj=>obj.id==id)
+    var sample_values = samples.sample_values
+    var otu_ids = samples.otu_ids
+    var otu_labels = samples.otu_labels
+        // console.log(samples)
+    var bar_data = [{
+        type: 'bar',
+        x: sample_values.slice(0,10).reverse(),
+        y: otu_ids.map(otu_id=>`OTU ${otu_id}`).slice(0,10).reverse(),
+        text: otu_labels.slice(0,10).reverse(),
+        orientation: 'h'
+      }];
+      
+      Plotly.newPlot('bar', bar_data);
+      
+      var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+          color: otu_ids,
+        //   opacity: [1, 0.8, 0.6, 0.4],
+          size: sample_values
+        }
+      };
+      
+      var bubble_data = [trace1];
+      
+      var bubble_layout = {
+        title: 'Marker Size and Color',
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
+      
+      Plotly.newPlot('bubble', bubble_data, bubble_layout);
+      
 }
 
 start()
